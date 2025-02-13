7270628dbb84a4a0405d99fefc23e82c569fb540
Rafal2228 <rafal2228@gmail.com>
2025-02-13 14:45:09 +0100

    Add unit test setup with default account

--- app/page.spec.tsx
+++ app/page.spec.tsx
@@ -1,11 +1,41 @@
-import { render } from '@testing-library/react';
+import {
+  connectWithDefaultUser,
+  defaultAccount,
+  render,
+  screen,
+  waitFor,
+} from '@/lib/test-utils';
 import { describe, expect, it } from 'vitest';
 import HomePage from './page';
 
 describe('HomePage', () => {
-  it('renders initial text', () => {
-    const { getByText } = render(<HomePage />);
+  it('renders logo and connect button', () => {
+    const { getByText, container } = render(<HomePage />);
 
-    expect(getByText('SWAP PAGE')).toBeVisible();
+    expect(getByText('Connect Wallet')).toBeVisible();
+    expect(
+      container.querySelector('svg[aria-label="Balancer Simple Swap"]'),
+    ).toBeVisible();
+  });
+
+  it('should allow user to select wallet', async () => {
+    const { getByText, getByRole } = render(<HomePage />);
+
+    getByText('Connect Wallet').click();
+    await waitFor(() => {
+      expect(getByRole('dialog')).toBeVisible();
+    });
+  });
+
+  it('should show user address after connection', async () => {
+    render(<HomePage />);
+    await connectWithDefaultUser();
+
+    expect(screen.queryByText('Connect Wallet')).toBeNull();
+    expect(
+      screen.getByText(
+        `${defaultAccount.substring(0, 4)}…${defaultAccount.substring(defaultAccount.length - 4)}`,
+      ),
+    ).toBeVisible();
   });
 });
---
+++ lib/test-utils.tsx
@@ -0,0 +1,59 @@
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  act,
+  render as baseRender,
+  RenderOptions,
+} from '@testing-library/react';
+import { PropsWithChildren, ReactElement } from 'react';
+import { createConfig, http, WagmiProvider } from 'wagmi';
+import { connect, disconnect } from 'wagmi/actions';
+import { arbitrum } from 'wagmi/chains';
+import { mock } from 'wagmi/connectors';
+
+export * from '@testing-library/react';
+
+const queryClient = new QueryClient();
+
+export const defaultAccount =
+  '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' as const;
+
+const config = createConfig({
+  chains: [arbitrum],
+  connectors: [
+    mock({
+      accounts: [defaultAccount],
+    }),
+  ],
+  storage: null,
+  transports: {
+    [arbitrum.id]: http(),
+  },
+});
+
+const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export const connectWithDefaultUser = () => {
+  return act(() => connect(config, { connector: config.connectors[0] }));
+};
+
+export const disconnectWithDefaultUser = () => {
+  return act(() => disconnect(config, { connector: config.connectors[0] }));
+};
+
+export const render = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) =>
+  baseRender(ui, {
+    wrapper: Providers,
+    ...options,
+  });
