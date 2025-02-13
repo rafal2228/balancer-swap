64a870992dbba53e24da38324259fbc89bdc9d3a
Rafal2228 <rafal2228@gmail.com>
2025-02-13 16:39:14 +0100

    Add extra webpack config for walletconnect compatibility

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
