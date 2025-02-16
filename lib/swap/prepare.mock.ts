b1b0ecc0b7210467b863a7fd3386f889b2b13b59
Rafal2228 <rafal2228@gmail.com>
2025-02-16 18:49:23 +0100

    Add prepare swap mocks

---
+++ lib/swap/prepare.mock.ts
@@ -0,0 +1,149 @@
+export const sorSwapPathsGivenIn = {
+  data: {
+    sorGetSwapPaths: {
+      tokenInAmount: '10000000',
+      tokenOutAmount: '3704498305960632',
+      returnAmount: '0.003704498305960632',
+      priceImpact: {
+        error: null,
+        priceImpact: '0.0034',
+      },
+      swapAmount: '10',
+      paths: [
+        {
+          inputAmountRaw: '2500000',
+          outputAmountRaw: '926624806998106',
+          pools: [
+            '0x423a1323c871abc9d89eb06855bf5347048fc4a5000000000000000000000496',
+            '0x178e029173417b1f9c8bc16dcec6f697bc323746000200000000000000000158',
+            '0xfb5e6d0c1dfed2ba000fbc040ab8df3615ac329c000000000000000000000159',
+          ],
+          isBuffer: [false, false, false],
+          protocolVersion: 2,
+          tokens: [
+            {
+              address: '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+              decimals: 6,
+            },
+            {
+              address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+              decimals: 6,
+            },
+            {
+              address: '0x5979d7b546e38e414f7e9822514be443a4800529',
+              decimals: 18,
+            },
+            {
+              address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+              decimals: 18,
+            },
+          ],
+        },
+        {
+          inputAmountRaw: '7500000',
+          outputAmountRaw: '2777873498962526',
+          pools: [
+            '0x2f0cdf8596be980ef24924ca7bf54e630ca526b2000000000000000000000529',
+            '0xdeeaf8b0a8cf26217261b813e085418c7dd8f1ee00020000000000000000058f',
+            '0xe8e800d94c2d1c2e7778115724cf49e811f60a460002000000000000000005e8',
+          ],
+          isBuffer: [false, false, false],
+          protocolVersion: 2,
+          tokens: [
+            {
+              address: '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+              decimals: 6,
+            },
+            {
+              address: '0x211cc4dd073734da055fbf44a2b4667d5e5fe5d2',
+              decimals: 18,
+            },
+            {
+              address: '0xca5d8f8a8d49439357d3cf46ca2e720702f132b8',
+              decimals: 18,
+            },
+            {
+              address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+              decimals: 18,
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+export const sorSwapPathsGivenOut = {
+  data: {
+    sorGetSwapPaths: {
+      tokenInAmount: '3139472682969373',
+      tokenOutAmount: '10000000',
+      returnAmount: '0.003139472682969373',
+      priceImpact: {
+        error: null,
+        priceImpact: '-0.1552',
+      },
+      swapAmount: '10',
+      paths: [
+        {
+          inputAmountRaw: '785400796215868',
+          outputAmountRaw: '2500000',
+          pools: [
+            '0xe8e800d94c2d1c2e7778115724cf49e811f60a460002000000000000000005e8',
+            '0x6ce1d1e46548ef657f8d7ebddfc4beadb04f72f30002000000000000000005a1',
+            '0x2e8ea681fd59c9dc5f32b29de31f782724ef4dcb0001000000000000000004bc',
+          ],
+          isBuffer: [false, false, false],
+          protocolVersion: 2,
+          tokens: [
+            {
+              address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+              decimals: 18,
+            },
+            {
+              address: '0xca5d8f8a8d49439357d3cf46ca2e720702f132b8',
+              decimals: 18,
+            },
+            {
+              address: '0x5979d7b546e38e414f7e9822514be443a4800529',
+              decimals: 18,
+            },
+            {
+              address: '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+              decimals: 6,
+            },
+          ],
+        },
+        {
+          inputAmountRaw: '2354071886753505',
+          outputAmountRaw: '7500000',
+          pools: [
+            '0xf13758d6edd1937dcb3f4fe75889b579d400299a000000000000000000000595',
+            '0xcdcef9765d369954a4a936064535710f7235110a000200000000000000000558',
+            '0x2e8ea681fd59c9dc5f32b29de31f782724ef4dcb0001000000000000000004bc',
+          ],
+          isBuffer: [false, false, false],
+          protocolVersion: 2,
+          tokens: [
+            {
+              address: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+              decimals: 18,
+            },
+            {
+              address: '0x35751007a407ca6feffe80b3cb397736d2cf4dbe',
+              decimals: 18,
+            },
+            {
+              address: '0x5979d7b546e38e414f7e9822514be443a4800529',
+              decimals: 18,
+            },
+            {
+              address: '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+              decimals: 6,
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
